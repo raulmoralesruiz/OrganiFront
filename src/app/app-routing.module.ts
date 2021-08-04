63825a0405ab435fdca9a4c7e4abae3e838da4f4
Raúl Morales <raulmdaw@gmail.com>
2021-08-04 19:31:14 +0200

    Correcciones leves

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: 'folder/Inbox',
-  //   pathMatch: 'full'
-  // },
   {
     path: '',
     loadChildren: () => import('./pages/item/welcome/welcome.module').then( m => m.WelcomePageModule)
   },
-  // {
-  //   path: 'folder/:id',
-  //   loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  // },
   {
     path: 'welcome',
     loadChildren: () => import('./pages/item/welcome/welcome.module').then( m => m.WelcomePageModule)
@@ -22,10 +13,6 @@ const routes: Routes = [
   {
     path: 'create',
     loadChildren: () => import('./pages/item/create/create.module').then( m => m.CreatePageModule)
-  },
-  {
-    path: 'show-all',
-    loadChildren: () => import('./pages/item/show-all/show-all.module').then( m => m.ShowAllPageModule)
   },
   {
     path: 'signup',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,8 @@ export class AppComponent {
   public appPages = [
     { title: 'Welcome', url: 'welcome', icon: 'search' },
     { title: 'New item', url: 'create', icon: 'add-circle' },
-    // { title: 'Show all items', url: 'show-all', icon: 'albums' },
     // { title: 'Sign up', url: 'signup', icon: 'log-in' },
     { title: 'Login', url: 'login', icon: 'key' },
-    // { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    // { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    // { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    // { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    // { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
